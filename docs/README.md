59979538ae21e342ecaf8671af7ae619b70e8de9
Nicholas Dunnaway <nick@dunnaway.org>
2021-12-20 12:56:20 -0800

    Mod Creation

--- docs/README.md
+++ docs/README.md
@@ -1,32 +1,11 @@
-Structuring your Thunderstore package
-https://github.com/ebkr/r2modmanPlus/wiki/Structuring-your-Thunderstore-package
-
-Manifest V1 (Thunderstore) + author field
-https://github.com/ebkr/r2modmanPlus/wiki/Installing-mods-locally#manifest-v1-thunderstore--author-field
-
-
-Move me to [gitrepo]/doc
-
 # Summary
-This is a port of <a href="https://www.nexusmods.com/valheim/mods/0000">ModTemplate</a> to JVL   
-PR: https://github.com/Atokal/AtosArrows/pull/3   
-Assets belong to <a href="https://github.com/Atokal" target="_blank">Atokal</a> and are used with permission because of:   
-- Asset use permission You are allowed to use the assets in this file without permission or crediting me. https://www.nexusmods.com/valheim/mods/969 (June 12, 2021)
- 
-Original Mod: https://www.nexusmods.com/valheim/mods/969
-
-**Code is a complete rewrite.**
+Valheim mod to mute Misophonia triggering sounds. 
 
 ## License
-Original source has no license.   
-JVL Version is released under: **GNU Affero General Public License v3.0**
+**GNU Affero General Public License v3.0**
 
-## ModTemplate
-Adds several new arrows into the game. Expands arrow progression for fighting tougher enemies! 
-
-New Items:
-- Stone Arrow
-- Blunted Arrow
+## Misophonia Friendly
+Mutes Misophonia triggering sounds. 
 
 ### Dependencies
 - <a href="https://valheim.thunderstore.io/package/denikson/BepInExPack_Valheim/"  target="_blank">BepInExPack Valheim</a>
@@ -43,9 +22,6 @@ New Items:
 - Use the repo issues tab above.
 
 ### Support Me @ https://www.buymeacoffee.com/digitalroot
-
-### Thanks to 
-- <a href="https://github.com/Atokal" target="_blank">Atokal</a>
 
 <p align="center">
 <b>Digitalroot can be found in the Odin Plus Team Discord</b><br />
---
+++ src/Digitalroot.Valheim.MisophoniaFriendly/Main.cs
@@ -0,0 +1,97 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Digitalroot.Valheim.Common;
+using HarmonyLib;
+using JetBrains.Annotations;
+using Jotunn.Managers;
+using Jotunn.Utils;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace Digitalroot.Valheim.MisophoniaFriendly
+{
+  [BepInPlugin(Guid, Name, Version)]
+  [BepInDependency(Jotunn.Main.ModGuid)]
+  public class Main : BaseUnityPlugin, ITraceableLogging
+  {
+    public const string Version = "1.0.1";
+    public const string Name = "Digitalroot Misophonia Friendly";
+    public const string Guid = "digitalroot.mods.misophoniafriendly";
+    public const string Namespace = "Digitalroot.Valheim" + nameof(MisophoniaFriendly);
+    private Harmony _harmony;
+    public static Main Instance;
+
+    public readonly ConfigEntry<int> NexusId;
+
+    public Main()
+    {
+      Instance = this;
+      NexusId  = Config.Bind("General", "NexusID",   1667, new ConfigDescription("Nexus mod ID for updates.", null, new ConfigurationManagerAttributes { IsAdminOnly = false, Browsable = false, ReadOnly = true }));
+      #if DEBUG
+      EnableTrace = true;
+      Log.RegisterSource(Instance);
+      #else
+      EnableTrace = false;
+      #endif
+      Log.Trace(Main.Instance, $"{Main.Namespace}.{MethodBase.GetCurrentMethod().DeclaringType?.Name}.{MethodBase.GetCurrentMethod().Name}");
+    }
+
+    [UsedImplicitly]
+    private void Awake()
+    {
+      try
+      {
+        Log.Trace(Main.Instance, $"{Main.Namespace}.{MethodBase.GetCurrentMethod().DeclaringType?.Name}.{MethodBase.GetCurrentMethod().Name}");
+        if (Common.Utils.IsHeadless()) return;
+        PrefabManager.OnVanillaPrefabsAvailable += PrefabManagerOnVanillaPrefabsAvailable;
+      }
+      catch (Exception e)
+      {
+        Log.Error(Instance, e);
+      }
+    }
+
+    [HarmonyWrapSafe]
+    private void PrefabManagerOnVanillaPrefabsAvailable()
+    {
+      try
+      {
+        Log.Trace(Main.Instance, $"{Main.Namespace}.{MethodBase.GetCurrentMethod().DeclaringType?.Name}.{MethodBase.GetCurrentMethod().Name}");
+
+        var prefabNames = new[]
+        {
+          Common.Names.Vanilla.PrefabNames.SfxPukeMale
+          , Common.Names.Vanilla.PrefabNames.SfxPukeFemale
+          , Common.Names.Vanilla.PrefabNames.SfxCreatureConsume
+          , Common.Names.Vanilla.PrefabNames.SfxEat
+        };
+
+        foreach (var prefabName in prefabNames)
+        {
+          var sfx = PrefabManager.Cache.GetPrefab<GameObject>(prefabName);
+          var audioSource = sfx.GetComponent<AudioSource>();
+          audioSource.mute = true;
+        }
+      }
+      catch (Exception e)
+      {
+        Log.Error(Instance, e);
+      }
+      finally
+      {
+        PrefabManager.OnVanillaPrefabsAvailable += PrefabManagerOnVanillaPrefabsAvailable;
+      }
+    }
+
+    #region Implementation of ITraceableLogging
+
+    /// <inheritdoc />
+    public string Source => Namespace;
+
+    /// <inheritdoc />
+    public bool EnableTrace { get; }
+
+    #endregion
+  }
+}
